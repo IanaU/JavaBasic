103b0d4de95416859572a847f40b1cf2eb280144
IanaU <ianaushko21@gmail.com>
2022-09-06 00:05:20 +0300

    Create HomeWork01.java

---
+++ HomeWork01.java
@@ -0,0 +1,41 @@
+/**
+ * Java Basic. Home work #1
+ *
+ * @ author Iana Ushko
+ * @ version 5.9.2022
+ *
+ */
+class HomeWork01 {
+    public static void main(String[] args) {
+        int a = 2;
+        System.out.println(a);
+        int b = 4;
+        System.out.println(b);
+        int p = 2 * (a + b);
+        System.out.println(p);
+        int s = a * b;
+        System.out.println(s);
+        System.out.printf("p = %d, s = %d", p, s);
+        char c = ',';
+        System.out.println(c);
+        int d = 5;
+        System.out.println(d);
+        int e = 3;
+        System.out.println(e);
+        int f = d + e;
+        System.out.println(f);
+        int g = d - e;
+        System.out.println(g);
+        int i = d * e;
+        System.out.println(i);
+        float h = d / 3f;
+        System.out.println(h);
+        int[] array = new int[5];
+        array[0] = 25;
+        array[1] = 12;
+        array[2] = 3;
+        array[3] = 55;
+        array[4] = 9;
+        System.out.printf("0 = %d, 1 = %d, 2 = %d, 3 = %d, 4 = %d", array[0], array[1], array[2], array[3], array[4]);
+    }
+}
